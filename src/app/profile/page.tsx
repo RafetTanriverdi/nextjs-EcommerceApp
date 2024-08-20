1a571d9985fde190903699feebd6c16b9576dce8
RafetTanriverdi <by.rafet.tanriverdi@gmail.com>
2024-08-20 16:09:25 +0300

    ssr config added for cognitoside

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,18 +1,10 @@
 import { Amplify } from "aws-amplify";
 import awsmobile from "../../aws-exports";
-import { checkUserAuthentication } from "../../utils/AuthUtils";
-import { GetServerSideProps } from "next";
 
 Amplify.configure(awsmobile, { ssr: true });
 
-interface ProfileProps {
-  user: {
-    username: string;
-  };
-}
-
-const profile = ({ user }: ProfileProps) => {
-  return <div>Profile {user ? user.username : "No user logged in"}</div>;
+const profile = () => {
+  return <div>Profile </div>;
 };
 
 export default profile;
