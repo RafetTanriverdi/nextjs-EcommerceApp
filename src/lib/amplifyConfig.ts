6030a2f90fe4e193c1e0cba560a83dcbe07d2007
RafetTanriverdi <by.rafet.tanriverdi@gmail.com>
2024-08-20 15:52:08 +0300

    ssr config added for cognitoside

--- src/lib/amplifyConfig.ts
+++ src/lib/amplifyConfig.ts
@@ -4,6 +4,7 @@ import { Amplify } from "aws-amplify";
 
 Amplify.configure({
   Auth: {
+    
     Cognito: {
       userPoolClientId: process.env.NEXT_PUBLIC_AWS_USER_POOL_CLIENT_ID!,
       userPoolId: process.env.NEXT_PUBLIC_AWS_USER_POOL_ID!,
